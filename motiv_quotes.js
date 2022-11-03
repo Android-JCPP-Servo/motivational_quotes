44e79926609cb568cc7d8fe0780280cc83263e41
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-03 09:03:03 -0500

    Replace IF/ELSE condition with Ternary Operator

--- motiv_quotes.js
+++ motiv_quotes.js
@@ -19,11 +19,7 @@ const randomSel = () => {
     // Run through array
     for (let i = 0; i < quotes.length; i++) {
         index = Math.floor(Math.random() * 6);
-        if (i == quotes.length - 1) {
-            message += quotes[index];
-        } else {
-            message += quotes[index] + " ";
-        }
+        i == quotes.length - 1 ? message += quotes[index] : message += quotes[index] + " ";
     }
     return message;
 }
